7c0043384808c1ef5bbb8747111f82c71f8ed6af
gprando55 <gprando55@gmail.com>
2022-04-10 22:04:27 -0300

    tentativa modularização

---
+++ Projeto_Oficina/codes/doorlock/main/setup_config.h
@@ -0,0 +1,13 @@
+#include <nvs_flash.h>
+#include <sys/param.h>
+
+void setup_init() {
+    //Initialize NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+}
