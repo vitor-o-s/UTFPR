def68f8c8923e622b11963c73d5ac5d91c272f8e
vitor-o-s <vitorsantos.2018@alunos.utfpr.edu.br>
2022-06-07 01:19:07 -0300

    Fix trab6

--- Sistemas_Operacionais/trabalho6.c
+++ Sistemas_Operacionais/trabalho6.c
@@ -28,117 +28,72 @@ A solução não deve ter nenhum comentário
 
 #define GAULESES 5
 #define JAVALIS 20
-#define ever ;;
 
+sem_t sem_Coz, sem_Retira, sem_Gau, sem_Lib, sem_mesa;
+
+int numJavalis = 0;
 char NOME[] = {'V', 'I', 'T', 'O', 'R'};
 
-pthread_mutex_t mutex;
-sem_t full, empty;
-pthread_t tid;
-
-typedef struct{
-    int num_javali;
-}Javali;
-
-int mesa[JAVALIS];
-int counter;
-
-
-void ComeJavali(char nome_thread, int tid){
-    printf("Gaules %c(%d) comendo\n", nome_thread,tid);
+void ColocaJavalis(){
+    numJavalis = JAVALIS;
 }
 
-int RetiraJavali(int *j){
-    if(counter > 0){ // When the buffer is not empty remove the item and decrement the counter
-		*j = mesa[(counter-1)];
-        //printf("Retirando %d",mesa[(counter-1)]);
-		counter--;
-		return 0;
-	}
-	else { // Error buffer empty
-		return 1;
-	}
-}
-    
-
-int ColocaJavalis(int M){
-    if(counter < JAVALIS){ // When the buffer is not full add the item and increment the counter*/
-		mesa[counter] = M;
-        //printf("%d",mesa[counter]);
-		counter++;
-        
-		return 0;
-	}
-	else{ // Error the buffer is full
-		return 1;
-	}
-}
-
-void* Gaules(void *id){
-    int item;
-    long tidd = (long)id;
-    while(1){
-        sem_wait(&full);
-        pthread_mutex_lock(&mutex);
-
-        
-        char nome_thread = NOME[tidd];
-
-        if(!RetiraJavali(&item)) {
-            //printf("Chegamo aqui\n");
-			ComeJavali(nome_thread, tidd);
-		}
-		else {
-			printf("Gaules %c(%ld) acordou o cozinheiro\n", nome_thread, tid);
-		}
-
-        //int j = RetiraJavali();
-        pthread_mutex_unlock(&mutex);
-        sem_post(&empty);
-        
+void RetiraJavali(long gaules){
+    sem_wait(&sem_Gau);
+    if(numJavalis == 0){
+        sem_post(&sem_Coz);
+        printf("Gaulês %c(%ld) acordou o cozinheiro\n", NOME[gaules], gaules);
+        sem_wait(&sem_Retira);
     }
-}
-
-void* Cozinheiro(){
-    while(1){
-        int M = rand() % 100;
-        sem_wait(&empty);
-        pthread_mutex_lock(&mutex);
-        if(!ColocaJavalis(M)) {
-			printf("Cozinheiro colocou javali na mesa \n");
-		}
-		/*else {
-			fprintf(stderr, " Producer report error condition\n");
-		}*/
-
-        pthread_mutex_unlock(&mutex);
-        sem_post(&full);
+    if(numJavalis){
+        numJavalis--;
     }
+    sem_post(&sem_Lib);
 }
 
-void initializeData() {
-	counter = 0; //init buffer
+void *Gaules(void *threadid){
+    long gaules = (long)threadid;
+    while(1){
+    	sem_wait(&sem_mesa);
+        sem_post(&sem_Gau);
+        RetiraJavali(gaules);
+        sem_wait(&sem_Lib);
+        printf("Gaulês %c(%ld) comendo\n", NOME[gaules], gaules);
+        sem_post(&sem_mesa);
+        sleep(rand() % 4 + 1);
+    }
+    pthread_exit(NULL);
+}
 
-	pthread_mutex_init(&mutex, NULL); //create the mutex lock */
-	sem_init(&full, 0, 0); 	//create the full semaphore and initialize to 0 */
-	sem_init(&empty, 0, JAVALIS); //create the empty semaphore and initialize to BUFFER_SIZE */
+void *Cozinheiro(){
+    while(1){
+        sem_wait(&sem_Coz);
+        printf("Cozinheiro reabasteceu os javalis e dormiu\n");
+        ColocaJavalis();
+        sem_post(&sem_Retira);
+    }
+    pthread_exit(NULL);
 }
 
 int main(void){
 
     int i;
-    pthread_t gauleses[GAULESES];
-    pthread_t cozinheiro;
+    pthread_t thread[GAULESES+1];
+    srand(time(NULL));
+    sem_init(&sem_Coz, 0, 0);
+    sem_init(&sem_Retira, 0, 0);
+    sem_init(&sem_Gau, 0, 0);
+    sem_init(&sem_Lib, 0, 0);
+    sem_init(&sem_mesa, 0, 1);
 
-	initializeData();
+    for(i = 0; i < GAULESES; i++)
+        pthread_create(&thread[i], NULL, Gaules, (void *)(intptr_t)i);
 
-	// Create the producer threads
-	pthread_create(&cozinheiro, NULL, Cozinheiro, NULL);
+    // Create the producer threads
+	pthread_create(&thread[GAULESES], NULL, Cozinheiro, NULL);
 
-    for(i = 1; i < GAULESES; i++)
-        pthread_create(&gauleses[i], NULL, Gaules, (void *)(intptr_t)i);
-
-    sleep(10);
-
+    for(i = 0; i <= GAULESES; i++)
+        pthread_join(thread[i], NULL);
+    
     return 0;
 }
